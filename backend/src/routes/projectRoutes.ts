c3fc2c8614934cba0c20febfc28d183e7b5ad38c
Gideon <gigi.onyegbula.2007@gmail.com>
2026-04-25 13:03:20 +0100

    refactor: removed unused internal backend.

--- backend/src/routes/projectRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getProjects } from '../controllers/projectController';
-
-const router = express.Router();
-
-// This will be accessible at /api/projects
-router.get('/', getProjects);
-
-export default router;
